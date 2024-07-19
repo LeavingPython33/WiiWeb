c9b3fba040fd21bb8efe709e1dfcd59d27c47675
Alfred <alfredtapiaratana@gmail.com>
2024-07-19 14:17:07 -0500

    Add proper zooming on TV

--- src/Components/TVSetup.js
+++ src/Components/TVSetup.js
@@ -1,5 +1,5 @@
-import { Cloud, Clouds, Center, Environment, Fisheye, OrbitControls, Text3D, Text, Grid, PerspectiveCamera, Bounds, useBounds } from '@react-three/drei'
-import React, { useEffect, useRef, useState } from 'react'
+import { Cloud, Clouds, Center, Environment, Fisheye, OrbitControls, Text3D, Text, Grid, PerspectiveCamera, Bounds, useBounds, Html } from '@react-three/drei'
+import React, { Suspense, useEffect, useRef, useState } from 'react'
 import { useFrame, useLoader, useThree } from '@react-three/fiber'
 import Tv from '../Models/Tv'
 import Table from '../Models/Table'
@@ -8,9 +8,9 @@ import CustomGrid from './CustomGrid'
 import * as THREE from 'three'
 import { useControls } from 'leva'
 
-function ChangeCamera(){
+function ChangeCamera(props){
   const bounds = useBounds()
-  const {
+  {/*const {
     moveX,
     moveY,
     moveZ,
@@ -32,12 +32,13 @@ function ChangeCamera(){
       upZ: 0,
       apply: false
     })
+  */}
   
   useEffect(() => {
     bounds.refresh().clip().fit()
     
-    if(apply){
-      bounds.moveTo([moveX, moveY, moveZ])
+    if(props.isZoom){
+      bounds.moveTo([1, 0.25, 0]).lookAt({ target: [0, 0.25, 0] })
     }
     //bounds.moveTo([moveX, moveY, moveZ]).lookAt({ target: [targetX, targetY, targetZ], up: [upX, upY, upZ] })
   })
@@ -45,10 +46,17 @@ function ChangeCamera(){
 
 export default function TVSetup() {
   const cloudRef = useRef()
-  const cameraRef = useRef()
+
 
   useFrame((state, delta) => {
     cloudRef.current.rotation.y += delta
+    
+    if(!isClicked){
+      tvRef.current.rotation.y += delta / 2
+    }
+    else{
+      tvRef.current.rotation.y = 0
+    }
     //console.log(state)
   })
 
@@ -64,16 +72,16 @@ export default function TVSetup() {
 
   return (
     <>
-      <perspectiveCamera ref={cameraRef}></perspectiveCamera>
-      <OrbitControls enableRotate={!isClicked} enableZoom={false} enablePan={false} zoom={5} minPolarAngle={-Math.PI / 2.5} maxPolarAngle={Math.PI / 2.25}></OrbitControls>
+      <perspectiveCamera makeDefault></perspectiveCamera>
+      {/*<OrbitControls makeDefault enableRotate={!isClicked} enableZoom={false} enablePan={false} zoom={5} minPolarAngle={-Math.PI / 2.5} maxPolarAngle={Math.PI / 2.25}></OrbitControls>*/}
       <ambientLight intensity={2}></ambientLight>
-
+      
       <group ref={tvRef} position={[0, -0.5, 0]}>
         <Bounds position={[0, 0, 0]} clip fit={true} observe margin={isClicked ? 1 : 3}>
           <group onClick={(event) => (event.stopPropagation(), setIsClicked(true))} onPointerMissed={() => (setIsClicked(false))} position={[0, 0.05, 0]}>
             <Center position={[0, 0.65, 0]} rotation={[0, -1.58, 0]}>
               <Tv castShadow></Tv>
-              <ChangeCamera></ChangeCamera>    
+              <ChangeCamera isZoom={isClicked}></ChangeCamera>    
             </Center>
             {/*<Text color="black" anchorX="center" anchorY="middle">
               hello world!
@@ -81,6 +89,10 @@ export default function TVSetup() {
             {/*<Table scale={0.15}></Table>*/}
             <SmallTable castShadow position={[0, -0.05, 0]} scale={0.75}></SmallTable>
           </group>
+
+          <Html occlude position={isClicked ? [0.195, 0.745, 0] : [0.19, 0.745, 0]} transform rotation={[0, Math.PI/2, 0]} scale={0.125} style={{backgroundColor: 'blue', width:'124px', height:"96px"}}>
+            <iframe src="https://www.alfredtapia.com/" ></iframe>
+          </Html>
         </Bounds>
         <CustomGrid></CustomGrid>
       </group>
